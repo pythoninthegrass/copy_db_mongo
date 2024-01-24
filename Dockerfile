d171ac34dad8185d4be0ec82f6b6c6ef62f5b8f8
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2024-01-24 16:40:23 -0600

    feat: import csv
    Successfully import CSVs via entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,19 @@
 # syntax=docker/dockerfile:1.6
 
-# use the official mongodb docker image
 FROM mongo:7.0.4-jammy
 
-# copy the csv files into the docker image
-COPY ./backup/*.csv /tmp/
-
-# environment variables for the mongodb instance
 ENV MONGO_INITDB_DATABASE=${DB_NAME}
-ENV MONGO_PORT=${PORT}
+ENV MONGO_INITDB_ROOT_USERNAME=${DB_USER:-root}
+ENV MONGO_INITDB_ROOT_PASSWORD=${DB_PASS:-root}
+ENV MONGO_PORT=${PORT:-27017}
 
-# set the working directory
 WORKDIR /app
 
-# TODO: qa
-# # install mongo cli
-# ENV DEBIAN_FRONTEND=noninteractive
-# RUN apt -qq update && apt -qq install -y mongodb-clients
+COPY ./backup/*.csv /app/backup
 
-# copy the script that will import the csv files into the mongodb instance
 COPY entrypoint.sh .
 
-# run the script when the docker image is run
-# CMD ["./entrypoint.sh"]
-CMD ["sleep", "infinity"]
+EXPOSE 27017
+
+# CMD ["sleep", "infinity"]
+CMD ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,117 @@
 #!/usr/bin/env bash
 
-# wait for the mongodb server to start
-sleep 10
+set -euo pipefail
+
+# env vars
+ENV=$(env | grep -E 'DB_NAME|DB_USER|DB_PASS')
+for var in $ENV; do
+	export $var
+done
+DB_NAME="${DB_NAME:-test}"
+DB_USER="${DB_USER:-root}"
+DB_PASS="${DB_PASS:-toor}"
+
+# check if mongod is already running
+check() {
+	if [[ -z $(pgrep mongod >/dev/null 2>&1; echo $?) ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+# stop the mongodb server
+stop() {
+	if [[ $(check) -eq 1 ]]; then
+		echo "Stopping mongod..."
+		pkill mongod
+	fi
+}
+
+# start the mongodb server in background
+start() {
+	stop
+	if [[ $# -eq 1 ]]; then
+		case $1 in
+			--auth)
+				echo "Starting mongod with access control..."
+				mongod --bind_ip_all --auth &
+				sleep 10
+				;;
+			--noauth)
+				echo "Starting mongod without access control..."
+				mongod --bind_ip_all --noauth &
+				sleep 10
+				;;
+			*)
+				echo "Invalid argument: $1"
+				exit 1
+				;;
+		esac
+	fi
+}
+
+# create a javascript file with the commands to create the user
+create_user() {
+if [[ ! -f "create_user.js" ]]; then
+	cat <<-EOF > create_user.js
+	use admin
+	db.createUser(
+	    {
+	        user: "${DB_USER}",
+	        pwd: "${DB_PASS}",
+	        roles: [
+	            { role: "root", db: "admin" },
+	        ]
+	    }
+	)
+	EOF
+fi
+	# run the JavaScript file with the mongo command
+	mongosh < create_user.js
+}
+
+# create database
+create_db() {
+	mongosh \
+		--username "${DB_USER}" \
+		--password "${DB_PASS}" \
+		--eval "db = db.getSiblingDB('${DB_NAME}'); db.createCollection('_temp')"
+}
+
+# remove all databases and collections
+reset_db() {
+	echo "Dropping all databases and collections..."
+	mongosh \
+		--username "${DB_USER}" \
+		--password "${DB_PASS}" \
+        --eval "db.getMongo().getDBNames().forEach(function(database) { if (database !== 'admin') { db.getSiblingDB(database).dropDatabase(); } });"
+}
 
 # import the csv files
-for filename in /tmp/*.csv; do
-	collection=${filename%.csv}
-	mongoimport --type csv \
-		--db "${DB_NAME}" \
-		--collection $collection \
-		--headerline \
-		--file $filename \
-		--drop
-done
+import_csv() {
+	for filename in ./backup/*.csv; do
+		collection=$(basename ${filename%.csv})
+		mongoimport \
+            --authenticationDatabase admin \
+			--username "${DB_USER}" \
+			--password "${DB_PASS}" \
+            --type csv \
+			--headerline \
+			--file "${filename}" \
+			--db "${DB_NAME}" \
+			--collection "${collection}" \
+			--drop
+	done
+}
 
-# start the mongodb server
-mongod
+main() {
+	start --noauth
+	create_user
+	start --auth
+	reset_db
+	import_csv
+}
+main "$@"
+
+exit 0
